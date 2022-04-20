8147faf546715fda44109d42a7fbea5f85569fc6
Mantas Kelminskas <mantas.kelminskas@gmail.com>
2022-04-20 18:58:27 +0300

    Adminas menu

--- wedish/wedish_menu/admin.py
+++ wedish/wedish_menu/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from .models import MenuModel, CategoryModel, ManuItemModel
 
-# Register your models here.
+
+admin.site.register(MenuModel)
+admin.site.register(CategoryModel)
+admin.site.register(ManuItemModel)
