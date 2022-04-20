da60eff488a9c21ca7282e8e172070d28c484314
Mantas Kelminskas <mantas.kelminskas@gmail.com>
2022-04-20 21:02:28 +0300

    changed model names

--- wedish/wedish_menu/admin.py
+++ wedish/wedish_menu/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
-from .models import MenuModel, CategoryModel, ManuItemModel
+from .models import Menu, Category, ManuItem
 
 
-admin.site.register(MenuModel)
-admin.site.register(CategoryModel)
-admin.site.register(ManuItemModel)
+admin.site.register(Menu)
+admin.site.register(Category)
+admin.site.register(ManuItem)
--- wedish/wedish_menu/models.py
+++ wedish/wedish_menu/models.py
@@ -1,10 +1,10 @@
-from django.utils.timezone import now
 from django.db import models
+from django.utils.timezone import now
 from django.utils.translation import gettext_lazy as _
 from treebeard.mp_tree import MP_Node
 
 
-class MenuModel(models.Model):
+class Menu(models.Model):
     name = models.CharField(_('Name'), max_length=30,
         help_text=_('ex.: Day menu, Festive menu, Ordinary menu'))
     valid_from = models.DateField(_('Valid from'), null=True, blank=True, db_index=True, default=now)
@@ -19,25 +19,25 @@ class MenuModel(models.Model):
 
     class Meta:
         ordering = ['name', 'valid_from']
-        verbose_name = _('Menu Model')
-        verbose_name_plural = _('Menu Models')
+        verbose_name = _('Menu')
+        verbose_name_plural = _('Menus')
 
     def __str__(self) -> str:
         return f'{self.name} - {self.publicity}'
 
 
-class CategoryModel(MP_Node):
+class Category(MP_Node):
     name = models.CharField(_('Name'), max_length=30,
         help_text=_('ex.: Salad, Roast, Pizza...'))
     node_order_by = ['name']
-    menu = models.ForeignKey(MenuModel, on_delete=models.CASCADE, null=True, verbose_name=_('Menu'))
+    menu = models.ForeignKey(Menu, on_delete=models.CASCADE, null=True, verbose_name=_('Menu'))
 
     def __str__(self):
         return 'Category: {}'.format(self.name)
 
 
-class ManuItemModel(models.Model):
+class ManuItem(models.Model):
     name = models.CharField(_('Name'), max_length=30)
     price = models.DecimalField(_('Price'), max_digits=10, decimal_places=2, blank=True, null=True)
-    category_group = models.ForeignKey(CategoryModel, on_delete=models.CASCADE, null=True, verbose_name=_('Catyegory group'))
-    # item = models.ForeignKey(RecipyModel, on_delete=models.CASCADE, null=True, verbose_name=_('Item'))
+    category_group = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, verbose_name=_('Category group'))
+    # item = models.ForeignKey(Recipy, on_delete=models.CASCADE, null=True, verbose_name=_('Item'))
