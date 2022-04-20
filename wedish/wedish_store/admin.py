1bc95bd71ceba3021c90cc6889c8cce3651e8373
vincisda <80175143+vincisda@users.noreply.github.com>
2022-04-20 19:24:30 +0300

    Update admin.py

--- wedish/wedish_store/admin.py
+++ wedish/wedish_store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+from .models import Product, GenericProduct, Service, Unit, Brand
 
-# Register your models here.
+
+admin.site.register(Product, GenericProduct, Service, Unit, Brand)
+
+
+
