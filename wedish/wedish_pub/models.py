8bd6c3c91877c5e1640efddc5eaf10d16579b6a6
DANIILAS KRUPSKIS <7hello@gmail.com>
2022-04-21 19:26:41 +0300

    created pub app; added app to settings; added models

--- wedish/wedish_pub/models.py
+++ wedish/wedish_pub/models.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 
 class Space(models.Model):
     name = models.CharField(_('Name'), max_length=100)
-    space_category = models.ForeignKey('SpaceCategory', on_delete=models.SET_NULL, null=True, verbose_name='Space', related_name='space_categories')
+    space_category = models.ForeignKey('SpaceCategory', on_delete=models.SET_NULL, null=True, verbose_name='Space', related_name='spaces')
     accepts_for_production = models.BooleanField(_('Accepts for production'), default=False)
     description = HTMLField(_('Description'), max_length=1000, help_text=_('Short description of this space'))
 
@@ -16,7 +16,7 @@ class SpaceCategory(models.Model):
 
 
 class Table(models.Model):
-    public_identifier = models.IntegerChoices()
-    space = models.ForeignKey(Space, )
-    seats_number = models.IntegerField()
+    public_identifier = models.PositiveIntegerField(_('Public identifier', default=0))
+    space = models.ForeignKey(Space, on_delete=models.SET_NULL, null=True, verbose_name='Space', related_name='tables')
+    seats_number = models.PositiveIntegerField('Seats number', default=1)
 
