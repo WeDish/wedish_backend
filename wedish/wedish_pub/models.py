b5af0c17a54cbe3850505cade18d012ac6deca2e
DANIILAS KRUPSKIS <7hello@gmail.com>
2022-04-21 18:51:06 +0300

    created pub app, added to settings. created models for app

---
+++ wedish/wedish_pub/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from tinymce.models import HTMLField
+from django.utils.translation import gettext_lazy as _
+
+
+class Space(models.Model):
+    name = models.CharField(_('Name'), max_length=100)
+    space_category = models.ForeignKey('SpaceCategory', on_delete=models.SET_NULL, null=True, verbose_name='Space', related_name='space_categories')
+    accepts_for_production = models.BooleanField(_('Accepts for production'), default=False)
+    description = HTMLField(_('Description'), max_length=1000, help_text=_('Short description of this space'))
+
+
+class SpaceCategory(models.Model):
+    name = models.CharField(_('Space Category'), max_length=100)
+    description = HTMLField(_('Description'), max_length=1000, help_text=_('Short description of this space category'))
+
+
+class Table(models.Model):
+    public_identifier = models.IntegerChoices()
+    space = models.ForeignKey(Space, )
+    seats_number = models.IntegerField()
+
