f135f76fa951ebce6cd3c7555b97f9360e0a1422
vincisda <80175143+vincisda@users.noreply.github.com>
2022-04-25 18:37:42 +0300

    partly done

--- wedish/wedish_store/admin.py
+++ wedish/wedish_store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, GenericProduct, Service, Unit, Brand, Allergen
+from .models import Product, GenericProduct, Service, Unit, Brand #, Allergen
 from django.utils.translation import gettext_lazy as _
 
 class ProductAdmin(admin.ModelAdmin):
@@ -16,20 +16,20 @@ class ServiceAdmin(admin.ModelAdmin):
 
 class UnitAdmin(admin.ModelAdmin):
     search_fields = ('name',)
-    list_filter = ('kilograms', 'litre')
+    
 
 class BrandAdmin(admin.ModelAdmin):
     search_fields = ('name', 'description')
     
 
-class AllergenAdmin(admin.ModelAdmin):
-    search_fields = ('ALLERGEN_CATEGORIES')
+# class AllergenAdmin(admin.ModelAdmin):
+#     search_fields = ('ALLERGEN_CATEGORIES')
 
 admin.site.register(Product, ProductAdmin)
 admin.site.register(GenericProduct, GenericProductAdmin)
 admin.site.register(Service, ServiceAdmin)
 admin.site.register(Unit, UnitAdmin)
 admin.site.register(Brand, BrandAdmin)
-admin.site.register(Allergen, AllergenAdmin)
+# admin.site.register(Allergen, AllergenAdmin)
 
 
