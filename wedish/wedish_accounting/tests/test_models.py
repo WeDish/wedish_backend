d6d579c4babcb1b8c2d1041b79612dc8de00657f
vincisda <80175143+vincisda@users.noreply.github.com>
2022-04-27 20:36:09 +0300

    vat test added

--- wedish/wedish_accounting/tests/test_models.py
+++ wedish/wedish_accounting/tests/test_models.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
-from wedish_accounting.models import Payment, Order
+from wedish_accounting.models import Payment, Order, VAT
 from wedish_pub.models import Space, SpaceCategory
 
 
 class PaymentModelTest(TestCase):
 
-
     def setUp(self):
-        Payment.objects.create(payment_method='Cash', currency='EUR')
+        self.payment = Payment.objects.create(payment_method='Cash', currency='EUR')
+
+    def test_payment_creation(self):
+        payment = self.payment
+        self.assertTrue(isinstance(payment, Payment))
 
     def test_payment_method_content(self):
         payment=Payment.objects.get(id=1)
@@ -18,8 +21,24 @@ class PaymentModelTest(TestCase):
         payment=Payment.objects.get(id=1)
         expected_currency = f'{payment.currency}'
         self.assertEqual(expected_currency, 'EUR')
+        
 
+class VATModelTest(TestCase):
 
+    def setUp(self):
+        self.vat = VAT.objects.create(
+        unit_rate=21.00
+        )
+
+    def test_vat_creation(self):
+        vat = self.vat
+        self.assertTrue(isinstance(vat, VAT))
+
+    def test_unit_rate_content(self):
+        vat = self.vat
+        self.assertEqual(vat.unit_rate, 21.00)
+
+    
 
 
 
