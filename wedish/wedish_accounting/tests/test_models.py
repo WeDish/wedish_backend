6762b521aaa070b4c975efa4691797c1f79d4424
vincisda <80175143+vincisda@users.noreply.github.com>
2022-05-09 21:28:49 +0300

    str test added

--- wedish/wedish_accounting/tests/test_models.py
+++ wedish/wedish_accounting/tests/test_models.py
@@ -1,18 +1,14 @@
 from django.test import TestCase
-from wedish_accounting.models import Payment, Order, VAT, Bill
-from wedish_pub.models import Table
-
-
+from wedish_accounting.models import Payment, Order, Bill
 
 
 class PaymentModelTest(TestCase):
-
     def setUp(self):
         self.order = Order.objects.create(
             total_price=100.00
         )
         self.bill = Bill.objects.create(
-            order =self.order, #create order
+            order =self.order,   
             total_price=100,
             discount=10,
             tips=2.00
@@ -33,29 +29,8 @@ class PaymentModelTest(TestCase):
         expected_currency = f'{payment.currency}'
         self.assertEqual(expected_currency, 'EUR')
 
-        
-
-class VATModelTest(TestCase):
-
-    def setUp(self):
-        self.vat = VAT.objects.create(
-        rate=21.00
-        )
-
-    def test_vat_creation(self):
-        vat = self.vat
-        self.assertTrue(isinstance(vat, VAT))
-
-    def test_unit_rate_content(self):
-        vat = self.vat
-        self.assertEqual(vat.rate, 21.00)
-
-    
-
 
 class OrderModelTest(TestCase):
-
-
     def setUp(self):
         self.order= Order.objects.create(
             total_price=100.00,
@@ -68,4 +43,8 @@ class OrderModelTest(TestCase):
     def test_order_total_price_content(self):
         order = self.order
         self.assertEqual(order.total_price, 0)
+
+    def test_order_str(self):
+        order = self.order
+        self.assertEqual(str(order), "0")
 
