0bd7e70fd39c146310d3da0fee5569572c1b66cd
DANIILAS KRUPSKIS <7hello@gmail.com>
2022-05-26 12:26:22 +0300

    Added styling to the pages. Added custom css.

--- wedish/wedish/settings.py
+++ wedish/wedish/settings.py
@@ -141,7 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR, 'media')
 
