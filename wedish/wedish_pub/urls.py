bdbeda69f933f6559a1e60cd84261e75a5c09278
Mantas Kelminskas <mantas.kelminskas@gmail.com>
2022-05-26 21:48:38 +0300

    Small changes

--- wedish/wedish_pub/urls.py
+++ wedish/wedish_pub/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'wedish_pub'
 urlpatterns = [
-    path('table_booking/', views.table_booking, name='table_booking'),
+    path('table_booking/', views.TableBooking.as_view(), name='table_booking'),
 ]
     
     
--- wedish/wedish_pub/views.py
+++ wedish/wedish_pub/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import Table
 
 
-@login_required
-def table_booking(request):
-    return render(request, 'wedish_pub/table_booking.html')
+class TableBooking(LoginRequiredMixin, CreateView):
+    model = Table
+    fields = ['public_identifier', 'space']
+    template_name = 'wedish_pub/table_booking.html'
+    success_url = reverse_lazy('wedish_pub:table_booking')
